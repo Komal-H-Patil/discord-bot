7ad39115883baf94eb011be061468aa0f3153673
Komal Patil <85443063+Komal-H-Patil@users.noreply.github.com>
2021-09-18 22:22:46 +0530

    Update main.py
    Discord Bot Quotes Feature Added

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import discord
 import os
+import requests
+import json
 
-client = discord.Client()
+
+
+client =discord.Client()
+
+def get_quote():
+  response = requests.get('https://zenquotes.io/api/random')
+  json_data =json.loads(response.text)
+  quote=json_data[0]['q'] +" -"+ json_data[0]['a']
+  return (quote)
+
+
 
 @client.event
-
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
+  print('we have logged in as{0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-      return
-    
-    if message.content.startwith('!hello'):
-      await message.channel.send('Hello')
+  if message.author== client.user:
+    return
 
-client.run(os.getenv('TOKEN'))
+  
+
+  if message.content.startswith('!quotes'):
+    quote=get_quote()
+    await message.channel.send(quote)
 
 my_secret = os.environ['TOKEN']
+client.run(os.getenv('TOKEN'))
